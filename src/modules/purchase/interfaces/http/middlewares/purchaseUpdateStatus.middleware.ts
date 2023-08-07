1ef73f411c7a2a8199c8193f3e0b469aa229c442
Crisstian Mella <crisstian.mella@gmail.com>
2023-08-06 23:25:52 -0400

    Add purshase update active param body validations

--- src/modules/purchase/interfaces/http/middlewares/purchaseUpdateStatus.middleware.ts
+++ src/modules/purchase/interfaces/http/middlewares/purchaseUpdateStatus.middleware.ts
@@ -5,7 +5,7 @@ import { StatusPurchare } from '../../../domain/enums/purchaseStatus.enum'
 
 class PurchaseMiddleware {
    static async ValidateUpdateStatus(req: Request, _res: Response, next: NextFunction) {
-      const { status } = req.body
+      const { status, active } = req.body
       const purchaseUpdateStatusValidator = new PurchaseUpdateStatusValidator()
       purchaseUpdateStatusValidator.status = status
       const errors = await validate(purchaseUpdateStatusValidator)
@@ -16,8 +16,15 @@ class PurchaseMiddleware {
          return next(new Error('Body update validation failed'))
       }
 
+      // Verificar si el parametro "status" proveniente del body es un valor valido permitido
       if (!Object.values(StatusPurchare).includes(status)) {
          return next(new Error('Status update parameter validation failed'))
+      }
+
+      //Verificar que el parametro "active" proveniente del body es un booleano
+
+      if (typeof active !== 'boolean') {
+         return next(new Error('Active update parameter validation failed'))
       }
 
       next()
